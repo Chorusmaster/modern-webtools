d7b342178cb7328a50da0880968d8b1150442d18
Chorusmaster <200482154+Chorusmaster@users.noreply.github.com>
2026-01-16 21:51:24 +0100

    css variant added

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../variants/html/views/HomeView.vue'
-import BasketView from '../variants/html/views/BasketView.vue'
+import HomeView from '../variants/css/views/HomeView.vue'
+import BasketView from '../variants/css/views/BasketView.vue'
 import CodeView from '../views/CodeView.vue'
 
 const router = createRouter({
