224d954f8213b070832f8c4244dbee08650acee5
Chorusmaster <200482154+Chorusmaster@users.noreply.github.com>
2026-01-18 02:11:08 +0100

    Updates

---
+++ assets/CodeView-BMMOGFjf.js
@@ -0,0 +1,10 @@
+import{C as t}from"./CodeTemplate-Boas5zVe.js";import{_ as s,c as o,r as a,o as n}from"./index-ZiKQSoAr.js";const r={components:{CodeTemplate:t}};function i(d,l,c,m,p,f){const e=a("CodeTemplate");return n(),o(e,{technology:"Tailwind",list:["Tailwind CSS is, as is evidently from name, framework for CSS","It allows to write predefined html classes instead of css rulesets","It is very flexible and customizable, that allows to do almost everything that plain CSS allows but faster"],code:`<div class="bg-[#222121]! m-4! w-70! min-w-70! transition-transform! duration-300! hover:scale-103!">
+  <img class="w-full! h-52! object-cover!" src="/img/products/bananas.jpg">
+  <div class="d-flex items-end! justify-between! p-2!">
+    <div class="p-3!">
+      <div class="card-header text-xl!">Bananas</div>
+      <div class="card-price text-base! text-[#AAAAAA]!">2.22$</div>
+    </div>
+    <button class="w-16! h-10! text-sm! bg-transparent! hover:bg-[#4de6000e]! border-none! rounded text-[#4DE600]!">BUY</button>
+  </div>
+</div>`})}const b=s(r,[["render",i]]);export{b as default};
