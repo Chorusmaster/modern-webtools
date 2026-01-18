ebc20540455492c9d7488ec6903d8b30559a4ae6
Chorusmaster <200482154+Chorusmaster@users.noreply.github.com>
2026-01-18 05:20:31 +0100

    Add files via upload

---
+++ assets/CodeView-Ckve6Qb-.js
@@ -0,0 +1,8 @@
+import{C as o}from"./CodeTemplate-DqjMorHE.js";import{_ as t,c as a,r as s,o as n}from"./index-Ct3iZf_9.js";const i={components:{CodeTemplate:o}};function p(r,c,l,d,m,u){const e=s("CodeTemplate");return n(),a(e,{technology:"HTML",list:["HTML (Hyper Text Markup Language) is a basic technology all modern web is built on","It's a markup language, that helps in building of site layout","No styles, no scripts yet, just simple markup"],code:`<div>
+  <img src="/img/products/bananas.jpg">
+  <div>Bananas</div>
+  <div>
+    10$
+    <button>BUY</button>
+  </div>
+</div>`})}const f=t(i,[["render",p]]);export{f as default};
---
+++ assets/bananas--_eoqI39.js
@@ -0,0 +1,1 @@
+const s="/img/products/bananas.jpg";export{s as _};
