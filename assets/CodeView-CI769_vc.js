bf5c5b3bf4a685880032b69d515f45c3b1acf1e5
Chorusmaster <200482154+Chorusmaster@users.noreply.github.com>
2026-01-18 14:22:05 +0100

    Updates

---
+++ assets/CodeView-CI769_vc.js
@@ -0,0 +1,8 @@
+import{C as o}from"./CodeTemplate-OPNvJKmh.js";import{_ as t,c as s,r as a,o as n}from"./index-CpLiSuE0.js";const i={components:{CodeTemplate:o}};function p(r,c,l,d,m,u){const e=a("CodeTemplate");return n(),s(e,{technology:"HTML",list:["HTML (Hyper Text Markup Language) is a basic technology all modern web is built on","It's a markup language, that helps in building of site layout","No styles, no scripts yet, just simple markup"],code:`<div>
+  <img src="@/assets/img/products/bananas.jpg">
+  <div>Bananas</div>
+  <div>
+    10$
+    <button>BUY</button>
+  </div>
+</div>`})}const f=t(i,[["render",p]]);export{f as default};
--- assets/CodeView-DosXV5ys.js
+++
@@ -1,8 +0,0 @@
-import{C as o}from"./CodeTemplate-D5E564Mt.js";import{_ as t,c as s,r as a,o as n}from"./index-DYVvmpwC.js";const i={components:{CodeTemplate:o}};function p(r,c,l,d,m,u){const e=a("CodeTemplate");return n(),s(e,{technology:"HTML",list:["HTML (Hyper Text Markup Language) is a basic technology all modern web is built on","It's a markup language, that helps in building of site layout","No styles, no scripts yet, just simple markup"],code:`<div>
-  <img src="@/assets/img/products/bananas.jpg">
-  <div>Bananas</div>
-  <div>
-    10$
-    <button>BUY</button>
-  </div>
-</div>`})}const f=t(i,[["render",p]]);export{f as default};
